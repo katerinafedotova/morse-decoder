03f4cd09ff93f1a81762b0a1d445c72e5b6ba654
Ekaterina <fedotova490@gmail.com>
2020-10-22 12:43:34 +0300

    feat: add solution to morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,17 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let array = []
+  for (let i = 0; i < expr.length; i+= 10){
+    array.push(expr.slice(i, i+10))
+  }
+
+  let arrayWithWhitespace = array.map(el => el === '**********' ? ' ' : el)
+  let morseArray = arrayWithWhitespace.map (el => el.replace(/10/g, '.')
+  .replace(/11/g, '-')
+  .replace(/00/g, ''))
+  let decodedArray = morseArray.map(el => el !== ' ' ? MORSE_TABLE[el] : ' ')
+  return decodedArray.join('') 
 }
 
 module.exports = {
